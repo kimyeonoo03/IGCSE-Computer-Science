a9599815adc83af7d3244f0e4ecd10153883c55a
kimyeonoo03 <yg111703@gmail.com>
2018-01-25 14:29:28 +0700

    First commit

---
+++ FirstTask.py
@@ -0,0 +1,4 @@
+lunch = ["Potato", "Noodles", "Spaghetti", "Beef", "Rice", "Pad Thai", "Pork"]
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+print (lunch[1])
