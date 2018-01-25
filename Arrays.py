ed7a28e0cafda70ea9e505aa9f4809d83b4d7dff
kimyeonoo03 <yg111703@gmail.com>
2018-01-25 14:44:25 +0700

    First commit

--- Arrays.py
+++ Arrays.py
@@ -1,4 +1,14 @@
 lunch = ["Potato", "Noodles", "Spaghetti", "Beef", "Rice", "Pad Thai", "Pork"]
 days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
-print (lunch[1])
+print (lunch[1]) # Look up an element
+
+'''
+When printing "Noodles" from this array in a pseudocode, the code is: print(lunch[2]) 
+'''
+# Linear Search
+
+date = input("Enter a date.")
+for day in range (7):
+    if date == days[day]:
+        print (lunch[day])
